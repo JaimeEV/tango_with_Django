a6eb61082e1d3e0e6ece181e3270e5950bc21616
Jaime Escobedo <maildeljimmy@gmail.com>
2022-07-06 14:20:31 -0500

    Ejercicio pag.27 resuelto

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-#from rango import views
+from rango import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
